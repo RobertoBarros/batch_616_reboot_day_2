9ae072b5b5011c3a3eea50afd8e6705c8be70fc9
RodrigoDonato <rodrigosdonato@outlook.com>
2021-04-27 10:40:51 -0400

    menu

--- interface.rb
+++ interface.rb
@@ -1,9 +1,22 @@
 # 1. Welcome
-
-# 2. Display menu (list / add / delete / mark )
-
-
-# 3. Get user action
-
-
-# 4. Perform the right action
+puts "Welcome"
+loop do
+  # 2. Display menu (list / add / delete / mark )
+  puts "Which action [list|add|delete|quit]?"
+  # 3. Get user action
+  action = gets.chomp
+  # 4. Perform the right action
+  case action
+  when "list"
+    puts "Lista de presentes"
+  when "add"
+    puts "adicionar presentes"
+  when "delete"
+    puts "deletando presente"
+  when "quit"
+    puts "good bye"
+    break
+  else
+    puts "comando nao e valido"
+  end
+end
