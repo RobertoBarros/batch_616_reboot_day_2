ca61924b1627bb426e762bfc3b6580f26a2a3d1c
Luciana Mascarenhas <lucianamascarenhas011@hotmail.com>
2021-04-27 14:11:27 -0300

    Save and load resolution.

--- interface.rb
+++ interface.rb
@@ -1,10 +1,29 @@
 require 'csv'
 
-FILENAME = 'gifts.txt'
+FILENAME = 'gifts.csv'
 CSV_OPTIONS = { col_sep: ',', quote_char: '"', headers: :first_row }
 
 
 gifts = []
+
+def save(gifts)
+  CSV.open(FILENAME, 'wb', CSV_OPTIONS) do |csv| #WB APAGA O ARQUIVO, salvando novamente.
+    # We had headers to the CSV
+    csv << ['name']
+    #TODO: store each gift
+    gifts.each do |gift|
+      csv << [gift]
+    end
+  end
+end
+
+def load(gifts)
+  CSV.foreach(FILENAME, CSV_OPTIONS) do |row|
+    # TODO: build new gift from information stored in each row
+    name = row['name']
+    gifts << name
+  end
+end
 
 def list(gifts)
   puts "-" * 40
@@ -18,6 +37,7 @@ def add(gifts)
   puts "Add a new gift:"
   gift = gets.chomp
   gifts << gift
+  save(gifts) #salvar todos os dados adicionados pelo usuario a lista.
   list(gifts)
 end
 
@@ -26,9 +46,11 @@ def delete(gifts)
   puts "Enter gift index to delete: "
   index = gets.chomp.to_i - 1
   gifts.delete_at(index)
+  save(gifts)
   list(gifts)
 end
 
+load(gifts) #carregar todos os dados salvos, sem o load sua lista permanecera vazia.
 # 1. Welcome
 puts "Welcome"
 loop do
